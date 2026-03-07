171ab3db8848500cf5ae9c1187279e6638165eb5
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-03-07 13:09:29 +0100

    feat(file): load .print file from path

--- scripts/main.py
+++ scripts/main.py
@@ -6,12 +6,20 @@ import os
 class Compiler:
 
     @staticmethod
+    def load_file(path: str) -> None:
+        file = Compiler.file_with_extension_exists(path, "print")
+        if file:
+            with open(file, 'r') as f:
+                code = file.read()
+        else:
+            print(f"Invalid path {path} to .print file.")
+
+    @staticmethod
     def file_with_extension_exists(path: str, extension: str) -> str | None:
         extension = extension if extension.startswith(".") else f".{extension}"
         if not path.endswith(extension):
             path += extension
         return path if os.path.exists(path) else None
-
 
     @staticmethod
     def run_code(path: str) -> None:
@@ -19,12 +27,12 @@ class Compiler:
         if file:
             subprocess.run([sys.executable, file])
         else:
-            raise FileNotFoundError(f"Invalid path {path} to .py file.")
+            print(f"Invalid path {path} to .py file.")
 
 
 def main() -> None:
 
-    app = Compiler()
+    Compiler.load_file(input("Path to root .print file: "))
 
 if __name__ == "__main__":
     main()
