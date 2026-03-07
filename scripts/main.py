5386c4570f2322e0ea4a4d4cb26a09806028da48
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-03-07 12:50:13 +0100

    feat(run): load file without extension
    Added ability to load file without specifying extension in the input.
    Added ability to load file without having to put dot in the start of the extension.
    Renamed main class to Compiler.

--- scripts/main.py
+++ scripts/main.py
@@ -3,19 +3,28 @@ import sys
 import os
 
 
-class Application:
+class Compiler:
+
+    @staticmethod
+    def file_with_extension_exists(path: str, extension: str) -> str | None:
+        extension = extension if extension.startswith(".") else f".{extension}"
+        if not path.endswith(extension):
+            path += extension
+        return path if os.path.exists(path) else None
+
 
     @staticmethod
     def run_code(path: str) -> None:
-        if os.path.exists(path):
-            subprocess.run([sys.executable, path])
+        file = Compiler.file_with_extension_exists(path, "py")
+        if file:
+            subprocess.run([sys.executable, file])
         else:
-            raise FileNotFoundError(f"Invalid path {path} to .print file.")
+            raise FileNotFoundError(f"Invalid path {path} to .py file.")
 
 
 def main() -> None:
 
-    app = Application()
+    app = Compiler()
 
 if __name__ == "__main__":
     main()
