36693c305371c2f6e61589039ceb90990d68c0a9
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-03-07 14:55:24 +0100

    feat(indent): add indentation to .print file
    Adds proper indentation when compiling from .print to .py. Practically the first functionality.

--- scripts/parser.py
+++ scripts/parser.py
@@ -3,8 +3,32 @@ class Parser:
     @staticmethod
     def parse_code(code: str) -> tuple[str, list[str]]:
         imported_files = []
-        for line in code.splitlines():
-            if line == "import more.test":
-                imported_files.append("../tests/more/test.print")
+        max_indent = Parser.get_biggest_indent(code)
+        code = Parser.indent_code(code, max_indent)
 
         return code, imported_files
+
+    @staticmethod
+    def get_biggest_indent(code: str) -> int:
+        indent = 0
+        for line in code.splitlines():
+            curr_indent = Parser.get_indent(line)
+            indent = curr_indent if curr_indent > indent else indent
+
+        return indent
+
+    @staticmethod
+    def get_indent(line: str) -> int:
+        return len(line) - len(line.lstrip(" "))
+
+    @staticmethod
+    def indent_code(code: str, indent: int) -> str:
+        result = ""
+        for line in code.splitlines():
+            to_indent = indent - Parser.get_indent(line)
+            result += " " * to_indent + line.lstrip(" ") + "\n"
+        return result
+
+if __name__ == "__main__":
+    with open("parser_test.print", "r") as f:
+        print(Parser.parse_code(f.read()))
