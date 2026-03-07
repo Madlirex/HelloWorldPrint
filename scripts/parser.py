5fd353b9fef1b32bb20dc583f578a253c3ea1740
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-03-07 16:03:00 +0100

    feat(parser)!: add custom slovak keywords
    Ternary operators not working yet. Works only on keywords at start of the line.
    
    Support for other keywords like in to be added in future.

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,8 +1,18 @@
 class Parser:
 
+    keywords = {
+        "čo ak": "if",
+        "ibaže": "elif",
+        "inak": "else",
+        "kým platí, že": "while",
+        "pre každé": "for",
+        "trieda": "class"
+    }
+
     @staticmethod
     def parse_code(code: str) -> tuple[str, list[str]]:
         imported_files = []
+        code = Parser.parse_lines(code)
         max_indent = Parser.get_biggest_indent(code)
         code = Parser.indent_code(code, max_indent)
 
@@ -30,6 +40,29 @@ class Parser:
         return result
 
     @staticmethod
+    def parse_lines(code: str) -> str:
+        result = ""
+        for line in code.splitlines():
+            result += Parser.parse_line(line) + "\n"
+        return result
+
+    @staticmethod
+    def parse_line(line: str) -> str:
+        indent = Parser.get_indent(line)
+        line = line.lstrip(" ")
+
+        line = Parser.parse_keywords(line)
+
+        return " " * indent + line
+
+    @staticmethod
+    def parse_keywords(line: str) -> str:
+        for key in Parser.keywords:
+            if line.startswith(key):
+                return Parser.keywords[key] + line.removeprefix(key)
+        return line
+
+    @staticmethod
     def parse_expression(func: str, arg: str) -> str:
         if func.startswith("f\""):
             result = f"{func[2:-1]}(f\"{arg}\")"
@@ -40,5 +73,5 @@ class Parser:
         return result
 
 if __name__ == "__main__":
-    with open("parser_test.print", "r") as f:
-        print(Parser.parse_code(f.read()))
+    with open("parser_test.print", "r", encoding="utf-8") as f:
+        print(Parser.parse_code(f.read())[0])
