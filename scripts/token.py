72221a1ef218fba065869ec8946cc08a00b4f023
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-03-08 20:16:14 +0100

    feat(token): add token types

--- scripts/token.py
+++ scripts/token.py
@@ -1,6 +1,16 @@
+from enum import Enum
+
+class TokenType(Enum):
+    INDENT = 0
+    KEYWORD = 1
+    OPERATION = 2
+    ARGUMENT = 3
+    FUNCTION = 4
+    KEYWORD_LINE_END = 5
+
 class Token:
 
-    def __init__(self, token_type: str, value: str, pos: tuple[tuple[int, int], tuple[int, int]]) -> None:
+    def __init__(self, token_type: TokenType, value: str, pos: tuple[tuple[int, int], tuple[int, int]]) -> None:
 
         self.token_type = token_type
         self.value = value
