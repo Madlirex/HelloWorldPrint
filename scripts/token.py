7dbbc129f3b533dc43d93a05ee87d53b3febf169
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-03-09 21:26:14 +0100

    feat(tokenizer): tokenize spaces?
    I don't know if it works, 0 tests made so far.

--- scripts/token.py
+++ scripts/token.py
@@ -9,6 +9,8 @@ class TokenType(IntEnum):
     OPERATION = 4
     ARGUMENT = 5
     FUNCTION = 6
+    COMMENT = 7
+    VALUE = 8
 
     @property
     def is_indent_next(self) -> bool:
@@ -20,10 +22,11 @@ class Token:
 
         self.token_type: TokenType = token_type
         self.value: str = value
+        self.int_value: int = 0
         self.position: tuple[tuple[int, int], tuple[int, int]] = pos
 
-    def add_to_value(self, x: str) -> None:
-        self.value += x
+    def add_to_value(self, value: str) -> None:
+        self.value += value
         self.position = self.position[0], (self.position[1][0] + 1, self.position[1][1])
 
     def translate_start_pos(self, x: int, y: int) -> None:
--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -1,17 +1,100 @@
 from token import Token, TokenType
 
+KEYWORDS = {
+        "čo ak": "if",
+        "ibaže": "elif",
+        "inak": "else",
+        "kým platí, že": "while",
+        "pre každé": "for",
+        "trieda": "class",
+        "definuje": "def",
+        "skús": "try",
+        "okrem": "except",
+        "na koniec": "finally",
+        "in": "v",
+        "sa rovná": "==",
+        "je menšie ako": "<",
+        "je väčšie ako": ">",
+        "je menšie alebo rovné ako": "<=",
+        "je väčšie alebo rovné ako": ">=",
+        "sa nerovná": "!=",
+        "neplatí": "not",
+        "alebo": "or",
+        "a": "and",
+        "je": "is",
+        "definuj": "def",
+        "zmaž": "del",
+        "vráť": "return",
+        "výnos": "yield",
+        "ako": "as",
+        "importuj": "import",
+        "z": "from",
+        "zlom": "break",
+        "pokračuj": "pass",
+        "preskoč": "continue",
+        "klamstvo": "False",
+        "pravda": "True",
+        "nič": "None",
+        "verejné": "global",
+        "nelokálna": "nonlocal",
+        "využi": "with",
+        "porovnaj": "match",
+        "v prípade, že": "case",
+        "vyvráť": "raise",
+        "skrátená funkcia, väčšinou anonymná a bez mena, používa sa pri krátkych operáciach alebo vo vnútri funkcií ako argument": "lambda"
+    }
+
 class Tokenizer:
+
+    curr_token: Token = Token()
+    result: list[Token] = []
+    is_string = False
+    is_comment = False
+    position: tuple[tuple[int, int], tuple[int, int]]
 
     @staticmethod
     def tokenize(code: str) -> list[Token]:
 
-        curr_token = Token()
-        curr_value = ""
-        result: list[Token] = []
+        curr_token = Token(token_type=TokenType.INDENT)
+        result: list[Token] = [curr_token]
 
         for char in code:
             if char == " ":
-                if curr_token.token_type.is_indent_next:
-                    pass
+                Tokenizer.tokenize_space()
+            else:
+                Tokenizer.curr_token.add_to_value(char)
 
         return result
+
+    # run away as fast as you can
+    @staticmethod
+    def tokenize_space() -> None:
+
+        if Tokenizer.result[-1].token_type.is_indent_next:
+            if Tokenizer.curr_token.token_type == TokenType.INDENT:
+                Tokenizer.curr_token.int_value += 1
+            else:
+                Tokenizer.result.append(Tokenizer.curr_token)
+                Tokenizer.curr_token = Token(TokenType.INDENT)
+
+        elif Tokenizer.is_string:
+            Tokenizer.curr_token.add_to_value(" ")
+
+        elif Tokenizer.is_comment:
+            Tokenizer.curr_token.add_to_value(" ")
+
+        elif Tokenizer.curr_token.token_type == TokenType.KEYWORD:
+            Tokenizer.result.append(Tokenizer.curr_token)
+            Tokenizer.curr_token = Token()
+
+        elif Tokenizer.curr_token.token_type == TokenType.ARGUMENT:
+            Tokenizer.curr_token.add_to_value(" ")
+
+        elif Tokenizer.curr_token.token_type == TokenType.UNKNOWN:
+            if Tokenizer.curr_token.value in KEYWORDS:
+                Tokenizer.curr_token.value = KEYWORDS[Tokenizer.curr_token.value]
+                Tokenizer.curr_token.token_type = TokenType.KEYWORD
+            else:
+                Tokenizer.curr_token.token_type = TokenType.VALUE
+                Tokenizer.result.append(Tokenizer.curr_token)
+                Tokenizer.curr_token = Token()
