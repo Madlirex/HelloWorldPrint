efcde852bcf4198f757a9ed36533171a0c8d7b31
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-03-10 21:01:31 +0100

    feat(tokenizer)!: revamp it's shit

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -47,100 +47,10 @@ KEYWORDS = {
 
 class Tokenizer:
 
-    curr_token: Token = Token()
-    result: list[Token] = []
-    is_string = False
-    is_comment = False
-    position: PositionRange = PositionRange()
-    curr_line: list[Token] = []
-
     @staticmethod
     def tokenize(code: str) -> list[Token]:
+        pass
 
-        curr_token = Token(token_type=TokenType.INDENT)
-        result: list[Token] = Tokenizer.result
-
-        for char in code:
-            if char == " ":
-                Tokenizer.tokenize_space()
-            elif char == "\n":
-                Tokenizer.tokenize_end_line()
-            elif char == ",":
-                Tokenizer.curr_token.token_type = TokenType.ARGUMENT
-                Tokenizer.curr_token.add_to_value(char)
-            elif char == "=":
-                Tokenizer.add_token(Tokenizer.curr_token)
-                Tokenizer.add_token(Token(token_type=TokenType.OPERATION, value="="))
-                Tokenizer.curr_token = Token(token_type=TokenType.VALUE)
-            elif Tokenizer.curr_token.token_type.is_indent_next:
-                Tokenizer.add_token(Tokenizer.curr_token)
-                Tokenizer.curr_token = Token(token_type=TokenType.UNKNOWN, value=char)
-            else:
-                Tokenizer.curr_token.add_to_value(char)
-
-        return result
-
-    @staticmethod
-    def get_latest_unknowns() -> list[Token]:
-        result = []
-        for t in Tokenizer.curr_line:
-            if t.token_type == TokenType.UNKNOWN:
-                result.append(t)
-            else:
-                result = []
-        return result
-
-    @staticmethod
-    def join_values(t: list[Token]) -> str:
-        return " ".join(i.value for i in t)
-
-    @staticmethod
-    def add_token(t: Token) -> None:
-        Tokenizer.result.append(t)
-        Tokenizer.curr_line.append(t)
-
-    @staticmethod
-    def tokenize_end_line() -> None:
-        Tokenizer.result.append(Tokenizer.curr_token)
-
-        if Tokenizer.curr_token.value.endswith(("!", "?", ".")):
-            char = Tokenizer.curr_token.remove_from_value(1)
-            Tokenizer.add_token(Token(token_type=TokenType.KEYWORD_LINE_END, value=char))
-
-        Tokenizer.curr_token = Token(token_type=TokenType.NEW_LINE, value="\n")
-        Tokenizer.curr_line = []
-
-
-    # run away as fast as you can
-    @staticmethod
-    def tokenize_space() -> None:
-
-        if Tokenizer.curr_token.token_type.is_indent_next:
-            if Tokenizer.curr_token.token_type == TokenType.INDENT:
-                Tokenizer.curr_token.int_value += 1
-            else:
-                Tokenizer.add_token(Tokenizer.curr_token)
-                Tokenizer.curr_token = Token(TokenType.INDENT)
-
-        elif Tokenizer.is_string or Tokenizer.is_comment:
-            Tokenizer.curr_token.add_to_value(" ")
-
-        elif Tokenizer.curr_token.token_type == TokenType.KEYWORD:
-            Tokenizer.add_token(Tokenizer.curr_token)
-            Tokenizer.curr_token = Token()
-
-        elif Tokenizer.curr_token.token_type == TokenType.ARGUMENT:
-            Tokenizer.curr_token.add_to_value(" ")
-
-        elif Tokenizer.curr_token.token_type == TokenType.UNKNOWN:
-            if Tokenizer.curr_token.value in KEYWORDS:
-                Tokenizer.curr_token.token_type = TokenType.KEYWORD
-                Tokenizer.add_token(Tokenizer.curr_token)
-                Tokenizer.curr_token = Token()
-            else:
-                Tokenizer.curr_token.token_type = TokenType.VALUE
-                Tokenizer.add_token(Tokenizer.curr_token)
-                Tokenizer.curr_token = Token()
 data = open("../tests/helloworld.print", 'r', encoding='utf-8').read()
 
 for token in Tokenizer.tokenize(data):
