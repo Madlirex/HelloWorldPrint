284a793fc9314bfcef4ff9d5357be2a1a37f864e
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-04-09 22:06:43 +0200

    refactor: remove test file

--- tests/bin/helloworld.py
+++ tests/bin/helloworld.py
@@ -1,16 +1,5 @@
-import more.test
-meno, vek = input(ja or ty), input('"Ako sa volas?"')
-print(f'"{meno} je pekne meno"')
-if meno == "simon" or meno == "Matej":
-    x = 5 + 5
-elif x or y:
-    x = 7
-else:
-    x = 5
-
-while x == 3:
-    x = 4
-
-def print(cauko):
-    x = 3
-
+meno = input("Ako sa volas ")
+print(f"dobre meno {meno}")
+vek = input("A co vek? ")
+vek = input("A co vek? ")
+vek = input("A co vek? ")
