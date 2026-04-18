41c4c915eb7ad8a25ef19003414aeaccb566f025
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-04-18 18:47:02 +0200

    fix(tokenizer): rename the ? operator to @ with args and keyargs

--- scripts/tokenizing/tokenizer.py
+++ scripts/tokenizing/tokenizer.py
@@ -72,7 +72,7 @@ class Tokenizer:
                 self.tokens.append(self.read_number())
                 continue
 
-            if char.isalpha() or char == "_" or char == "?":
+            if char.isalpha() or char == "_" or char == "@":
                 self.tokens.append(self.read_identifier())
                 continue
 
@@ -203,9 +203,9 @@ class Tokenizer:
 
         name = ""
 
-        while self.peek() and (self.peek().isalnum() or self.peek() == "_" or self.peek() == "?"):
+        while self.peek() and (self.peek().isalnum() or self.peek() == "_" or self.peek() == "@"):
             char = self.advance()
-            name += "*" if char == "?" else char
+            name += "*" if char == "@" else char
 
         return Token(TokenType.VALUE, name)
 
--- tests/bin/helloworld.py
+++ tests/bin/helloworld.py
@@ -1,2 +1,17 @@
-family = ["hi", "mama", "dada", ]
-print(family[1::])
+import tkinter as tk
+class Application():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Tic Tac Toe")
+        self.root.geometry("600x600")
+        self.update()
+
+    def update(self):
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    Application()
+else:
+    pass
+
